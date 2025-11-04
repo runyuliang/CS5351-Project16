94af7c493b5068b894b6953fa6e78cec20385c87
sunleilei0723 <691257082@qq.com>
2025-11-05 00:53:15 +0800

    feat(backend): 设计用户和角色数据模型

---
+++ cs5351-project/backend/app/models/role.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from ..db.base import Base
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String, nullable=True)
